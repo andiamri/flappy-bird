0618bf13e2c7d9647d1141489703d71c1530fa63
Andi Amri <56806919+andiamri@users.noreply.github.com>
2020-03-02 09:40:29 +0700

    Adding Ground Control
    Ground Can Now Move To Left According To Speed

---
+++ FlappyBird/Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class GroundSpawner : MonoBehaviour
+{
+    [SerializeField] private Ground groundRef;
+
+    private Ground prevGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SpawnGround()
+    {
+        if(prevGround != null)
+        {
+            Ground newGround = Instantiate(groundRef);
+
+            newGround.gameObject.SetActive(true);
+            prevGround.SetNextGroundObject(newGround.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Ground ground = collision.GetComponent<Ground>();
+
+        if (ground)
+        {
+            prevGround = ground;
+
+            SpawnGround();
+        }
+    }
+}
