0df5bd1d9922ce4f5f1c48bd98c7dcfa38f40dc1
Andi Amri <56806919+andiamri@users.noreply.github.com>
2020-03-02 09:06:33 +0700

    Add Bird Controller
    Adding Bird Controller

--- FlappyBird/Assets/Scripts/Bird.cs
+++ FlappyBird/Assets/Scripts/Bird.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Bird : MonoBehaviour
 {
+    [SerializeField] private float upForce = 300;
+    [SerializeField] private bool isDead;
+    [SerializeField] private UnityEvent OnDead, OnJump;
+
+    private Animator anim;
+    private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isDead && Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+            anim.enabled = true;
+        }
+        else
+        {
+            anim.enabled = false;
+        }
+    }
+    public bool IsDead()
+    {
+        return isDead;
+    }
+    public void Dead()
+    {
+        if(!isDead && OnDead != null)
+        {
+            OnDead.Invoke();
+        }
+        isDead = true;
+    }
+    public void Jump()
+    {
+        if (rigid)
+        {
+            rigid.velocity = Vector2.zero;
+
+            rigid.AddForce(new Vector2(0, upForce));
+        }
+        if(OnJump != null)
+        {
+            OnJump.Invoke();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Dead();
+        anim.enabled = false;
     }
 }
